8cbae8b204fb0d4059341ef8eca061daa1907083
91aashutosh <aashu2348154@gmail.com>
2024-06-12 12:22:24 +0530

    pushing some changes

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -1,44 +1,46 @@
-const express = require('express');
-const router = express.Router();
-const Customer = require('../models/Customer');
-const Order = require('../models/Order');
+  const express = require('express');
+  const router = express.Router();
+  const Customer = require('../models/Customer');
+  const Order = require('../models/Order');
 
-// Create a new customer
-router.post('/', async (req, res) => {
-  try {
-    const newCustomer = new Customer(req.body);
-    const customer = await newCustomer.save();
-    res.json(customer);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+  // Create a new customer
+  router.post('/', async (req, res) => {
+    try {
+      const newCustomer = new Customer(req.body);
+      const customer = await newCustomer.save();
+      res.json(customer);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
 
-// Get customers with filters
-router.get('/', async (req, res) => {
-  const { spends, visits, noVisitMonths } = req.query;
-  const query = {};
+  // Get customers with filters
+  router.post('/getList', async (req, res) => {
+    const { spends, visits, noVisitMonths } = req.body;
+    const query = {};
 
-  if (spends) {
-    query.totalSpends = { $gt: spends };
-  }
+    console.log(spends, visits, noVisitMonths)
+    if (spends) {
+      query.totalSpends = { $gte: spends };
+    }
 
-  if (visits) {
-    query.visits = { $eq: visits };
-  }
+    if (visits) {
+      query.visits = { $gte: visits };
+    }
 
-  if (noVisitMonths) {
-    const date = new Date();
-    date.setMonth(date.getMonth() - noVisitMonths);
-    query.lastVisitDate = { $lt: date };
-  }
+    if (noVisitMonths) {
+      const date = new Date();
+      date.setMonth(date.getMonth() - noVisitMonths);
+      query.lastVisitDate = { $lte: date };
+    }
 
-  try {
-    const customers = await Customer.find(query);
-    res.json(customers);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+    try {
+      const customers = await Customer.find(query);
+      console.log("customers", customers);
+      res.json(customers);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
 
-module.exports = router;
+  module.exports = router;
