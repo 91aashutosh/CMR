38d3cfeaf85d0e8a23c0cf64031f39f1555873dc
91aashutosh <aashu2348154@gmail.com>
2026-05-10 21:25:28 +0530

    Update server.js to set host to '0.0.0.0' for better accessibility

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 // const kafkaConsumer = require('./kafka/consumer');
 
 const port = process.env.PORT || 5000;
-const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || '0.0.0.0';
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
